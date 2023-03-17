c0c3b97fbedc654f7870122db843b5621effcc92
vick4 <victorisaacblw@gmail.com>
2023-03-17 16:05:43 -0700

    dee

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,16 +18,16 @@ int main(void)
 
 		for (j = 1; j < 10; j++)
 		{
-		if (i < j && i != j)
-		{
-			putchar(i + '0');
-			putchar(i + '0');
-			if (i + j != 17)
+			if (i < j && i != j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-		}
 		}
 	}
 	putchar('\n');
