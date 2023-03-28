09c20c0b70bbc5a68f01049181b8b6bcac3d128b
vick4 <victorisaacblw@gmail.com>
2023-03-27 19:19:06 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - print strings
+ * @str:function parameter
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
