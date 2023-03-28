5b57d26c0d4ebdb32d6d0b7b82eca4c74e3abe4b
vick4 <victorisaacblw@gmail.com>
2023-03-27 20:00:06 -0700

    done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,32 @@
 #include "main.h"
 /**
- * puts2 - print strings
- * @str:function parameter
- * Return: 0
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
 
 void puts2(char *str)
 {
-	int i;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	int o;
+
+	while (*y != '\0')
+
 	{
-		_putchar(str[i]);
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
 	}
 	_putchar('\n');
 }
