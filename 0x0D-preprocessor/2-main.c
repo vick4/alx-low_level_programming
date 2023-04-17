623831e06c9fd7d503ea9d8367f2d85898c4ed7a
vick4 <victorisaacblw@gmail.com>
2023-04-17 12:25:35 -0700

    third task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - kindly print name of the file where it compliation took place
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
