6759498b4fcb485da8a4a3ac9fb5d502210d9071
vick4 <victorisaacblw@gmail.com>
2023-04-27 13:07:32 -0700

    count list element

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * count_list_elements - counts the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	
+	return count;
+}
